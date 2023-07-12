605995c1c4be5df4d2d8201a3dc06acd51623267
Gabriel-sys-hub <gabrielfernandessilva.ba@gmail.com>
2023-07-12 15:29:11 -0300

    feat: exercicio da primeira aula, sobre variaveis e seus tipos

---
+++ src/main/kotlin/ExercicioVariaveis.kt
@@ -0,0 +1,8 @@
+import java.math.BigInteger
+
+fun main() {
+    var nomeCompleto = "Gabriel Pimentel Fernandes"
+    var nome: String
+    val numeroDoCalcado: Short = 42
+    var pibBrasil: Long = 1869000000000
+}
