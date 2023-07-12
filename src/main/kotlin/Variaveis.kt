a4d8fe41ffaabcf3439ff1bbb4a988c346f19887
Gabriel-sys-hub <gabrielfernandessilva.ba@gmail.com>
2023-07-12 15:19:50 -0300

    feat: variaveis mutaveis e imutaveis

--- src/main/kotlin/Variaveis.kt
+++ src/main/kotlin/Variaveis.kt
@@ -2,4 +2,11 @@ fun main() {
     var nome: String = "Gabriel Pimentel Fernandes";
     var idade: Number = 28;
     println("Meu nome é $nome e tenho $idade")
+
+    //variáveis mutaveis e imutaveis
+
+    nome = "José" //variável mutavel, pode ser alterada
+    println("Agora você é $nome")
+
+    val nomeReal: String = "Gabriel" //variável imutavel, não pode ser alterada
 }
